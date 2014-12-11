03ef61723d56124be8cb8f53fed5848d8da68184
Mike Gaughan <michael.t.gaughan@gmail.com>
2014-12-10 23:42:10 -0700

    rspec and capybara set up

---
+++ spec/features/navigation_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'Navigation Menu', js: true do
+	
+	it 'changes active menu element depending on route' do
+		visit '/'
+		expect(find('.active').find('a').text).to eq('Home')
+
+		click_link('Edges')
+		expect(find('.active').find('a').text).to eq('Edges')
+	end
+
+end
